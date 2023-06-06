e31e53ba838e7d7d3edd343ddd908e80962b77d9
BackDevBin <tishansarker873@gmail.com>
2023-06-06 14:30:03 +0600

    Implement Post Method:insertone toy data

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ async function run() {
         console.log(toy);
         res.send(result);
       })
+
+      // app.delete('/toys/:id',async(req, res) =>{
+      //   const id = req.params.id;
+      //   const query = {_id : new ObjectId(id)};
+      //   const deleteItem = await toyData.deleteOne(query);
+      //   res.send(deleteItem);
+      // })
+  
   
   
 
